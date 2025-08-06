bbddf9f613384d1158d8aa3870fa690963b989dd
ddayofs <ddayofs@gmail.com>
2025-08-07 00:42:30 +0900

    CREATE 강의까지

--- src/App.js
+++ src/App.js
@@ -41,14 +41,31 @@ function Article(props) {
     )
 }
 
+function Create(props) {
+    return <article>
+        <h2>Create</h2>
+        <form onSubmit={event=>{
+            event.preventDefault();
+            const title = event.target.title.value; //해당 event를 발생시킨 태그 중 name이 title인 태그의 value값을 가져온다
+            const body = event.target.body.value;
+            props.onCreate(title,body);
+        }}>
+            <p><input type="text" name="title" placeholder="title"></input></p>
+            <p><textarea name="body" placeholder="body"></textarea></p>
+            <p><input type="submit" value="Create"></input></p>
+        </form>
+    </article>
+}
+
 function App() {
     const [mode, setMode] = useState('WELCOME');
     const [id, setId] = useState(null);
-    const topics = [
+    const [nextId, setNextId] = useState(4);
+    const [topics, setTopics] = useState([
         {id:1, title:'html', body:'html is ...'},
         {id:2, title:'css', body:'css is ...'},
         {id:3, title:'javascript', body:'javascript is ...'},
-    ]
+    ]);
 
     let content = null;
     if (mode === 'WELCOME') {
@@ -64,6 +81,17 @@ function App() {
             }
         }
         content = <Article title={title} body={body}></Article>;
+    } else if(mode === 'CREATE') {
+        content=<Create onCreate={(_title,_body )=>{
+            const newTopic= {id:nextId, title:_title, body:_body}
+            const newTopics = [...topics];
+            newTopics.push(newTopic);
+            setTopics(newTopics);
+            setMode('READ');
+            setId(nextId);
+            setNextId(nextId + 1);
+
+        }}></Create>
     }
         
   return (
@@ -80,6 +108,10 @@ function App() {
         {/* <Article title="I am props.title"body= "I am props.body"></Article> */}
         {/* <Article title="Welcome"body= "Hello, Web"></Article> */}
         {content}
+        <a href="/create" onClick={event=>{
+            event.preventDefault();
+            setMode('CREATE');
+        }}>Create</a>
     </div>
   );
 }
