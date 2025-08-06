b6c26f83a7c25391aa8a1229e61521851634bac2
ddayofs <ddayofs@gmail.com>
2025-08-06 23:28:58 +0900

    react-app copied

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function Header(props) {
+    console.log('props', props.title)
+    return (
+        <header>
+            <h1><a href="/" onClick={(event)=>{
+                event.preventDefault();
+                props.onChangeMode();
+            }}>{props.title}</a></h1>
+        </header>
+    )    
+}
+
+function Nav(props) {
+    const lis = []
+    for (let i=0; i<props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(<li key={t.id}><a href={'/read'+t.id} id={t.id}onClick={(event)=> {
+            event.preventDefault();
+            props.onChangeMode(Number(event.target.id));
+        }}>{t.title}</a></li>)
+    }
+    return (
+        <nav>
+            <ol>
+                {lis}
+            </ol>
+        </nav>
+    )
+}
+
+function Article(props) {
+    return (
+        <article>
+            <h2>{props.title}</h2>
+            {props.body}
+        </article>
+    )
+}
+
+function App() {
+    const [mode, setMode] = useState('WELCOME');
+    const [id, setId] = useState(null);
+    const topics = [
+        {id:1, title:'html', body:'html is ...'},
+        {id:2, title:'css', body:'css is ...'},
+        {id:3, title:'javascript', body:'javascript is ...'},
+    ]
+
+    let content = null;
+    if (mode === 'WELCOME') {
+        content = <Article title='WELCOME' body='Hello, WEB'></Article>;
+    } else if(mode === 'READ') {
+        let title, body = null;
+        for(let i = 0; i < topics.length; i++) {
+            console.log(id,topics[i].id);
+            if (id === topics[i].id){
+                title = topics[i].title;
+                body = topics[i].body;
+                break;
+            }
+        }
+        content = <Article title={title} body={body}></Article>;
+    }
+        
+  return (
+    <div>
+        <Header title="WEB" onChangeMode={function(){
+            // alert('Header');
+            setMode('WELCOME');
+        }}></Header>
+        <Nav topics={topics} onChangeMode={(_id)=>{
+            // alert(id);
+            setMode('READ');
+            setId(_id);
+        }}></Nav>
+        {/* <Article title="I am props.title"body= "I am props.body"></Article> */}
+        {/* <Article title="Welcome"body= "Hello, Web"></Article> */}
+        {content}
+    </div>
+  );
+}
+
+export default App;
